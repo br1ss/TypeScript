2d9fc38219aeeea7b9d519a956c297f07b017c2f
Sheetal Nandi <shkamat@microsoft.com>
2020-10-14 17:42:12 -0700

    sourceFileToPackageName to keyed with Path

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3875,7 +3875,7 @@ namespace ts {
         /* @internal */ getLibFileFromReference(ref: FileReference): SourceFile | undefined;
 
         /** Given a source file, get the name of the package it was imported from. */
-        /* @internal */ sourceFileToPackageName: ESMap<string, string>;
+        /* @internal */ sourceFileToPackageName: ESMap<Path, string>;
         /** Set of all source files that some other source file redirects to. */
         /* @internal */ redirectTargetsMap: MultiMap<Path, string>;
         /** Is the file emitted file */
@@ -3970,7 +3970,7 @@ namespace ts {
         getFileProcessingDiagnostics(): readonly ReusableFilePreprocessingDiagnostics[] | undefined;
 
         redirectTargetsMap: MultiMap<Path, string>;
-        sourceFileToPackageName: ESMap<string, string>;
+        sourceFileToPackageName: ESMap<Path, string>;
         structureIsReused?: StructureIsReused;
     }
 
