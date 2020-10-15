bc924b9360631b54ffaf9d1f7d264124e94b28fb
Sheetal Nandi <shkamat@microsoft.com>
2020-10-14 17:33:30 -0700

    redirectsTarget is keyed with Path

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3877,7 +3877,7 @@ namespace ts {
         /** Given a source file, get the name of the package it was imported from. */
         /* @internal */ sourceFileToPackageName: ESMap<string, string>;
         /** Set of all source files that some other source file redirects to. */
-        /* @internal */ redirectTargetsMap: MultiMap<string, string>;
+        /* @internal */ redirectTargetsMap: MultiMap<Path, string>;
         /** Is the file emitted file */
         /* @internal */ isEmittedFile(file: string): boolean;
         /* @internal */ getFileIncludeReasons(): MultiMap<Path, FileIncludeReason>;
@@ -3969,7 +3969,7 @@ namespace ts {
         isSourceFileFromExternalLibraryPath(path: Path): boolean;
         getFileProcessingDiagnostics(): readonly ReusableFilePreprocessingDiagnostics[] | undefined;
 
-        redirectTargetsMap: MultiMap<string, string>;
+        redirectTargetsMap: MultiMap<Path, string>;
         sourceFileToPackageName: ESMap<string, string>;
         structureIsReused?: StructureIsReused;
     }
@@ -3995,7 +3995,7 @@ namespace ts {
     export type ReusableFilePreprocessingDiagnostics = ReusableFilePreprocessingReferencedDiagnostic | ReusableFilePreprocessingFileExplainingDiagnostic;
 
     /* @internal */
-    export type RedirectTargetsMap = ReadonlyESMap<string, readonly string[]>;
+    export type RedirectTargetsMap = ReadonlyESMap<Path, readonly string[]>;
 
     export interface ResolvedProjectReference {
         commandLine: ParsedCommandLine;
